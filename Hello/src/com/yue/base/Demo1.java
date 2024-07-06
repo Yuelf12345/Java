2bc0362b7e9c2e6d284335206bf6613a324f9d85
yue <2794127100@qq.com>
2024-07-06 16:59:38 +0800

    java 基础

---
+++ Hello/src/com/yue/base/Demo1.java
@@ -0,0 +1,7 @@
+package com.yue.base;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Demo1!");
+    }
+}
--- Hello/src/com/yue/base/Hello.java
+++ Hello/src/com/yue/base/Hello.java
@@ -1,6 +1,10 @@
+package com.yue.base;
+
+import com.yue.base.Demo1;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("com.yue.base.Hello World!");
 //        内存溢出
         int money = 1_000_000_000;
         int years = 20;
